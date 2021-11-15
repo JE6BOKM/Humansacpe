f5f927ab649011897101eb25102331731a90bf78
sungmuk Kang <miranaky@gmail.com>
2021-11-15 18:59:40 +0900

    FEAT: Create Clinial Information Model
    - 질병관리청_임상연구 과제정보 모델에 따라 모델 정의
    -  추가되는 데이터를 확인하기 위해 created_at 필드를 추가하여 시간 정보 확인

--- apps/config/common.py
+++ apps/config/common.py
@@ -34,6 +34,7 @@ class Common(Configuration):
         # Your apps
         "apps.users",
         "apps.core",
+        "apps.humanscape",
     )
 
     # django-alauth
---
+++ apps/humanscape/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+
+@admin.register("ClinicalInfo")
+class ClinicalInfoAdmin(admin.ModelAdmin):
+    """Clinical Info Admin"""
+
+    list_display = (
+        "proejct_number",
+        "proejct_name",
+        "department",
+        "responsible_institution",
+        "total_target_number",
+        "research_duration",
+        "research_type",
+        "clinical_trial_stage",
+        "research_scope",
+    )
