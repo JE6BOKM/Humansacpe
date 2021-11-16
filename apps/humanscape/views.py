7fdfa1fcfd87942b6145d14cfeb81bf0adfd8efb
sungmuk Kang <miranaky@gmail.com>
2021-11-16 23:03:46 +0900

    HOTFIX: search with trail_name and fix urls problem

--- apps/humanscape/views.py
+++ apps/humanscape/views.py
@@ -1,7 +1,6 @@
 from django_filters import rest_framework as filters
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from apps.humanscape.filters import ClinialInfoRecentlyUpdateFilter
@@ -16,12 +15,6 @@ class ClinicalInfoViewset(RetrieveModelMixin, ListModelMixin, GenericViewSet):
     queryset = ClinicalInfo.objects.all()
     serializer_class = ClinicalInfoSerialzers
     permission_classes = [AllowAny]
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object(kwargs["pk"])
-        serializer = self.get_serializer(instance)
-
-        return Response(serializer.data)
 
     def filter_queryset(self, queryset):
         if self.action == "list":
